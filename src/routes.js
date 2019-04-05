0333c3f68ce9ae22bdb821131a3c04d128eb486d
ronaldscruz <ronald.scruz16@gmail.com>
2019-04-05 05:59:48 -0300

    conflitos arrumados

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,10 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'
 
 import Main from './pages/main'
 import HeroDetail from './pages/heroDetail'
-<<<<<<< HEAD
 import Search from './pages/search'
 
 import Icon from 'react-native-vector-icons/FontAwesome'
-import {Button, Text, TouchableOpacity} from 'react-native'
-=======
-
-import {Button} from 'react-native'
->>>>>>> 9e8842fb7b7926d276a38522f59d568ce6d710d3
+import {TouchableOpacity} from 'react-native'
 
 const rootStack = createStackNavigator({
    Main: {
@@ -20,34 +15,19 @@ const rootStack = createStackNavigator({
       navigationOptions: ({navigation}) => ({
          title: 'MARVEL_HUB',
          headerRight: (
-<<<<<<< HEAD
-            // <Button
-            //    onPress={() => navigation.navigate('/search')}
-            //    title= <Icon name="search" color="white"/> + "Search"
-            //    color="#0e1111"
-            // />
             <TouchableOpacity style={{marginHorizontal: 15}} onPress={() => navigation.navigate('Search')}>
                <Icon name="search" color="white" size={20}/>
             </TouchableOpacity>
-=======
-            <Button
-               onPress={() => alert('Função em andamento')}
-               title="Search"
-            />
->>>>>>> 9e8842fb7b7926d276a38522f59d568ce6d710d3
          )
       })
    },
    HeroDetail: {
       screen: HeroDetail,
       path: '/hero/:id',
-<<<<<<< HEAD
    },
    Search: {
       screen: Search,
       path: '/search'
-=======
->>>>>>> 9e8842fb7b7926d276a38522f59d568ce6d710d3
    }
 }, {
    defaultNavigationOptions: {
